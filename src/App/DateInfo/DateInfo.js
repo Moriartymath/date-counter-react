3656f8ba8d1cdfa6763268bcefcad15048203883
Moriarty <116695498+Moriartymath@users.noreply.github.com>
2024-06-29 05:54:19 +0300

    removing unnecessary useState hook

--- src/App/DateInfo/DateInfo.js
+++ src/App/DateInfo/DateInfo.js
@@ -7,7 +7,6 @@ const now = new Date();
 
 function DateInfo() {
   const [date, setDate] = useState(new Date());
-  const [text, setText] = useState(`Today is ${date.toDateString()}`);
   const [stepLen, setStepLen] = useState(1);
   const [count, setCount] = useState(0);
 
@@ -15,22 +14,17 @@ function DateInfo() {
     setDate(new Date(now - -count * 24 * 60 * 60 * 1000));
   }, [count]);
 
-  useEffect(() => {
-    const newText = `${
-      count === 0
-        ? "Today is"
-        : count > 0
-        ? `${count} days from today is`
-        : `${Math.abs(count)} days ago was`
-    } ${date.toDateString()}`;
-    setText(newText);
-  }, [date]);
-
   return (
     <>
       <StepCounter setStepLen={setStepLen} stepLen={stepLen} />
       <DateCounter stepLen={stepLen} count={count} setCount={setCount} />
-      <p>{text}</p>
+      <p>{`${
+        count === 0
+          ? "Today is"
+          : count > 0
+          ? `${count} days from today is`
+          : `${Math.abs(count)} days ago was`
+      } ${date.toDateString()}`}</p>
     </>
   );
 }
